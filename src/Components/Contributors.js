29675c32913a4d8c1ba0845e5170bd1a4ec9a257
unai gonzalez <unaigoe91@gmail.com>
2021-03-26 14:32:42 +0100

    eror handling enhanved

--- src/Components/Contributors.js
+++ src/Components/Contributors.js
@@ -3,20 +3,18 @@ import React from 'react'
 
 export const Contributors = ({ data }) => {
     return (
-        <div>
+        <div className='contributors--container'>
             <h2 className='contributors-h2'>Contributors</h2>
             {
                 data !== undefined &&
                 data.map((item, index) => {
                     return (
-                        <div key={index} className='get-contr-container'>
-                            <div className='get-contr-card'>
-                                <img className='user-pic' src={item.avatar_url} alt='user-pic' />
-                                <span className='contributions'>{item.contributions} contributions</span>
-                                <span className='contributor-git'><a className='contributor-git' href={`${item.html_url}`}>Contributor's Github</a></span>
-                                <p className='contributor-p'>Github User</p>
-                                <p className='p-child'>{item.login}</p>
-                            </div>
+                        <div key={index} className='d-flex flex-column justify-content-center align-content-center text-center border-1 border-white'>
+                                <img className='user-pic d-block mx-auto' src={item.avatar_url} alt='user-pic' />
+                                <span className='text-white'>{item.contributions} contributions</span>
+                                <span className=''><a className='' href={item.html_url}>Contributor's Github</a></span>
+                                <p className='text-white'>Github User</p>
+                                <p className='text-white'>{item.login}</p>
                         </div>
 
                     );
--- src/Components/GetContributors.js
+++ src/Components/GetContributors.js
@@ -13,50 +13,57 @@ export const GetContributors = () => {
     const [repo, setRepo] = useState('');
     const [infoSent, setInfoSent] = useState(false);
     const [error, setError] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
 
     const getContributors = async () => {
         try {
             setInfoSent(true);
+            setError(false);
             const result = await axios.get(`https://api.github.com/repos/${owner}/${repo}/contributors`);
             setData(result.data);
         } catch (error) {
-            setInfoSent(false)
-            setError(true)
+            setInfoSent(false);
+            setError(true);
+            if (error.response) {
+                if(error.response.status === 403){
+                    setErrorMessage('You have exceeded the amount of API calls')
+                } else{
+                    setErrorMessage(`The repository or owner you are looking for does not exist`)
+                }
+            } else if (error.request) {
+                setErrorMessage(`There has been an error while retrieving data, please try again.`)
+            } else {
+                setErrorMessage(error)
+            }
         }
     };
 
     return (
-        <div className='app-container'>
-            {
-                <>
-                    <div>
-                        <h2 className='contributors-h2'>Search Github Repository</h2>
-                        <form className='form'>
-                            <input
-                                value={owner}
-                                onChange={e => setOwner(e.target.value)}
-                                className='form-input'
-                                placeholder="Type Github Repository's owner"
-                                type="text"
-                                name="owner"
-                                required
-                            />
-                            <input
-                                value={repo}
-                                onChange={e => setRepo(e.target.value)}
-                                className='form-input'
-                                placeholder="Type  Github Repository's name"
-                                type="text"
-                                name="repo"
-                                required
-                            />
-                            {error && <p className='text-danger fw-bold'>This repo or owner does not exist </p>}
-                            <button type='button' className='btn-search' onClick={() => getContributors()}>Search Contributors</button>
-                        </form>
-                    </div>
-                    { infoSent && <Contributors data={data} />}
-                </>
-            }
-        </div>
+        <main className='app-container'>
+                    <h2 className='contributors-h2'>Search Github Repository</h2>
+                    <form className='form'>
+                        <input
+                            value={owner}
+                            onChange={e => setOwner(e.target.value)}
+                            className='form-input'
+                            placeholder="Type Github Repository's owner"
+                            type="text"
+                            name="owner"
+                            required
+                        />
+                        <input
+                            value={repo}
+                            onChange={e => setRepo(e.target.value)}
+                            className='form-input'
+                            placeholder="Type  Github Repository's name"
+                            type="text"
+                            name="repo"
+                            required
+                        />
+                        {error && <p className='text-danger fw-bold'>{errorMessage}</p>}
+                        <button type='button' className='btn-search' onClick={() => getContributors()}>Search Contributors</button>
+                    </form>
+                    {infoSent && <Contributors data={data} />}
+        </main>
     )
 }
